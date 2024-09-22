0a4bb5aecff40a58e59ed889872f483d5d4d0fb6
shuayyy <nshuaibmohamed@gmail.com>
2024-09-22 18:14:13 +0530

    adde3d yaml param

--- ros_ws1/build/p2/colcon_command_prefix_setup_py.sh
+++ ros_ws1/build/p2/colcon_command_prefix_setup_py.sh
@@ -1,2 +1,1 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/gopi/ros_simpleproj/ros_ws1/install/p2/share/p2/package.sh"
--- ros_ws1/src/p2/launch/pubsub.launch.py
+++ ros_ws1/src/p2/launch/pubsub.launch.py
@@ -1,7 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
-TOPIC = "chatter";
+#TOPIC = "chatter";
+#get directory of package
+
+package_share_dir = get_package_share_directory('p2')
+    
+#path to yaml configuration file
+    
+config_file = os.path.join(package_share_dir, 'config', 'param.yaml')
+
+
 
 def generate_launch_description():
     return LaunchDescription([
@@ -10,13 +21,15 @@ def generate_launch_description():
             executable='publisher',  
             name='publisher_node',
             output='screen',
-            parameters=[{"topic":TOPIC}],
+            #parameters=[{"topic":TOPIC}],
+            parameters=[config_file]
         ),
         Node(
             package='p2',  
             executable='subscriber',  
             name='subscriber_node',
             output='screen',
-             parameters=[{"topic":TOPIC}],
+            #parameters=[{"topic":TOPIC}],
+            parameters=[config_file]
         ),
     ])
